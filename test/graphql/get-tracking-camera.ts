16168bcf115c5e673a0f4f86f52ebac6c3560176
Hearty Oh <heartyoh@hatiolab.com>
2020-06-18 18:00:12 +0900

    Create get-tracking-camera.ts

---
+++ test/graphql/get-tracking-camera.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+import '../../server/graphql'
+
+describe('CameraStreamer', function () {
+  before(function () {})
+
+  after(function () {})
+
+  describe('listen', function () {
+    it('should connectable from websocket client', done => {})
+  })
+})
