a1bf4676c5592b3875e7199b728130813aa19de5
vincent841 <vincent841@naver.com>
2020-07-15 10:49:32 +0900

    fix something wrong about getting a robotarm pose

--- server/graphql/resolvers/robot-arm/robot-arm-pose.ts
+++ server/graphql/resolvers/robot-arm/robot-arm-pose.ts
@@ -2,11 +2,14 @@ import { VISION_OBJECT_TYPES } from '../../../controllers/vision-types'
 import { Connections } from '@things-factory/integration-base'
 
 export const robotArmPoseResolver = {
-  async robotArmPose(_: any, { name, pose }, context: any) {
+  async robotArmPose(_: any, { name }, context: any) {
     var connection = Connections.getConnection(name)
 
     if (connection && connection.discriminator == VISION_OBJECT_TYPES.ROBOT_ARM) {
-      return await connection.getTaskPos()
+      var [x, y, z, u, v, w] = await connection.getTaskPos()
+      return {
+        x, y, z, u, v, w
+      }
     } else {
       throw Error(`RobotArm '${name}' Not Found`)
     }
